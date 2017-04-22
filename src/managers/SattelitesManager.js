23e494cf56a1d34232a5551681b6d53a226efb27
Leo <leo@leo-sa.com>
2017-04-22 15:42:24 -0500

    Converted to use groups instead of ticks

--- src/managers/SattelitesManager.js
+++ src/managers/SattelitesManager.js
@@ -2,44 +2,20 @@ import Satellite from '../sprites/Satellite';
 import { getRandomInt } from '../utils';
 import Phaser from 'phaser';
 
-export const createNewSatellites = ({ state, beats, currentTick, distanceToThreshold }) => {
-    const beatErrorMargin = 0.01;
-    const speed = 100;
-    const startTick = currentTick + (distanceToThreshold / speed);
-    const matchingBeats =
-          beats.filter((beat) => beat > (startTick - beatErrorMargin) && beat < (startTick + beatErrorMargin));
-    const satellites =
-          matchingBeats.map(
-              (beat) => new Satellite({
-                  game: state,
-                  x: 0,
-                  y: getRandomInt(0, 200),
-                  asset: 'mushroom',
-                  speed: speed
-              })
-          );
-
-    satellites.forEach((satellite) => state.game.add.existing(satellite));
-    return satellites;
-};
-
-const calculateSatX = (beat, speed) =>
-    
-
 const createSatellites = (game, beats, thresholdX, speed) =>
     beats.map(
         (beat) => new Satellite({
             game: game,
-            x: calculateSatX(thresholdX, beat, speed),
+            x: thresholdX - beat * speed,
             y: getRandomInt(0, 200),
             asset: 'mushroom',
             speed: 0
         })
     );
 
-const createSatelliteGroup = (game, beats, thresholdX, speed) => {
-    const group = new Phaser.Group();
+export const createSatelliteGroup = (game, beats, thresholdX, speed) => {
+    const group = new Phaser.Group(game);
     createSatellites(game, beats, thresholdX, speed)
         .forEach((s) => group.addChild(s));
     return group;
-}
+};
