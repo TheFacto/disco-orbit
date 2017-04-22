71678cb0c90e08126cbc858e13ad9bc5cad6b3e7
WR020200 <will.reynolds@cerner.com>
2017-04-22 10:04:42 -0500

    New: Threshold class.
    * Need to figure out how to scale it to match world height
    * Eventually replace asset with a prettier sprite

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,13 +1,14 @@
 /* globals __DEV__ */
 import Planet from '../sprites/Planet';
 import Phaser from 'phaser';
-import Satellite from '../sprites/Satellite';
+import Threshold from '../sprites/Threshold';
 import { createNewSatellites } from '../managers/SattelitesManager';
 
 export default class extends Phaser.State {
     init () {
         this.satellites = [];
     }
+
     preload () {}
 
     create () {
@@ -16,11 +17,18 @@ export default class extends Phaser.State {
             x: this.world.width - 150,
             y: this.world.centerY
         });
+        this.threshold = new Threshold({
+            game: this,
+            x: this.world.width - 300,  // TODO: Calculate actual placement
+            y: this.world.centerY,
+            asset: 'threshold'
+        });
         this.game.add.existing(this.planet);
+        this.game.add.existing(this.threshold);
     }
 
     render () {
         this.satellites =
             this.satellites.concat(createNewSatellites(this, 100));
-  }
+    }
 }
