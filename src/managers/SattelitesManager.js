37ad22c4bcc56f81923a4dc4cd184f2fed4d8e57
lsa <leo@leo-sa.com>
2017-04-22 14:46:43 -0500

    Started revamp of the manager

--- src/managers/SattelitesManager.js
+++ src/managers/SattelitesManager.js
@@ -1,5 +1,6 @@
 import Satellite from '../sprites/Satellite';
 import { getRandomInt } from '../utils';
+import Phaser from 'phaser';
 
 export const createNewSatellites = ({ state, beats, currentTick, distanceToThreshold }) => {
     const beatErrorMargin = 0.01;
@@ -21,3 +22,24 @@ export const createNewSatellites = ({ state, beats, currentTick, distanceToThres
     satellites.forEach((satellite) => state.game.add.existing(satellite));
     return satellites;
 };
+
+const calculateSatX = (beat, speed) =>
+    
+
+const createSatellites = (game, beats, thresholdX, speed) =>
+    beats.map(
+        (beat) => new Satellite({
+            game: game,
+            x: calculateSatX(thresholdX, beat, speed),
+            y: getRandomInt(0, 200),
+            asset: 'mushroom',
+            speed: 0
+        })
+    );
+
+const createSatelliteGroup = (game, beats, thresholdX, speed) => {
+    const group = new Phaser.Group();
+    createSatellites(game, beats, thresholdX, speed)
+        .forEach((s) => group.addChild(s));
+    return group;
+}
